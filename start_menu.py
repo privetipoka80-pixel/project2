19f7cd165cec89488a3c1b4e4ea7e5766ca6d3cb
Vanechka-777 <vanya.chugunov2009@gmail.com>
2026-01-09 23:42:23 +0300

    добавление подсвечевания кнопок стартового окна

--- start_menu.py
+++ start_menu.py
@@ -6,6 +6,8 @@ class StartMenu(arcade.View):
         super().__init__()
         self.game_window_class = game_window_class
         self.background_color = arcade.color.BLACK
+        self.start_hovered = False
+        self.exit_hovered = False
 
     def on_show_view(self):
         arcade.set_background_color(self.background_color)
@@ -27,9 +29,12 @@ class StartMenu(arcade.View):
         start_y = self.window.height / 2
         exit_y = self.window.height / 2 - 70
 
-        # Кнопка СТАРТ
-        arcade.draw_lrbt_rectangle_filled(start_x - 100, start_x + 100, start_y - 25, start_y + 25, arcade.color.DARK_GREEN)
-        arcade.draw_lrbt_rectangle_outline(start_x - 100, start_x + 100, start_y - 25, start_y + 25, arcade.color.WHITE, 2)
+        start_color = arcade.color.GREEN if self.start_hovered else arcade.color.DARK_GREEN
+        exit_color = arcade.color.RED if self.exit_hovered else arcade.color.DARK_RED
+        border_width = 4 if self.start_hovered or self.exit_hovered else 2
+
+        arcade.draw_lrbt_rectangle_filled(start_x - 100, start_x + 100, start_y - 25, start_y + 25, start_color)
+        arcade.draw_lrbt_rectangle_outline(start_x - 100, start_x + 100, start_y - 25, start_y + 25, arcade.color.WHITE, border_width)
         arcade.draw_text(
             "СТАРТ",
             start_x,
@@ -40,9 +45,8 @@ class StartMenu(arcade.View):
             anchor_y="center"
         )
 
-        # Кнопка ВЫХОД
-        arcade.draw_lrbt_rectangle_filled(start_x - 100, start_x + 100, exit_y - 25, exit_y + 25, arcade.color.DARK_RED)
-        arcade.draw_lrbt_rectangle_outline(start_x - 100, start_x + 100, exit_y - 25, exit_y + 25, arcade.color.WHITE, 2)
+        arcade.draw_lrbt_rectangle_filled(start_x - 100, start_x + 100, exit_y - 25, exit_y + 25, exit_color)
+        arcade.draw_lrbt_rectangle_outline(start_x - 100, start_x + 100, exit_y - 25, exit_y + 25, arcade.color.WHITE, border_width)
         arcade.draw_text(
             "ВЫХОД",
             start_x,
@@ -52,6 +56,14 @@ class StartMenu(arcade.View):
             anchor_x="center",
             anchor_y="center"
         )
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        start_x = self.window.width / 2
+        start_y = self.window.height / 2
+        exit_y = self.window.height / 2 - 70
+
+        self.start_hovered = start_x - 100 < x < start_x + 100 and start_y - 25 < y < start_y + 25
+        self.exit_hovered = start_x - 100 < x < start_x + 100 and exit_y - 25 < y < exit_y + 25
 
     def on_mouse_press(self, x, y, button, modifiers):
         start_x = self.window.width / 2
@@ -70,5 +82,4 @@ class StartMenu(arcade.View):
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.ESCAPE:
-
             arcade.exit()
